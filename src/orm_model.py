a0abb831cafa6f2dc098965c7c3e24aa7b3e0445
jamesbarciz <Fauxpasla93>
2021-08-04 21:04:22 -0500

    Add APP to __init__.py

--- src/orm_model.py
+++ src/orm_model.py
@@ -11,7 +11,7 @@ class Author(DB.Model):
 
 
 class Tweet(DB.Model):
-
+    """A table (Tweet) containing an identifier (id), tweet text (body) and a foreign key from Author"""
     id = DB.Column(DB.BigInteger, primary_key=True, nullable=False)
     body = DB.Column(DB.Unicode(300))
     author_id = DB.Column(DB.BigInteger, DB.ForeignKey('author.id'), nullable=False)
